50b28f049188823a8353d52961478d909719be83
aurora682 <140974950+aurora682@users.noreply.github.com>
2024-09-17 23:42:08 -0600

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css'
+// })
+// export class LoginComponent {
+
+// }
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Login Successful', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
+}
